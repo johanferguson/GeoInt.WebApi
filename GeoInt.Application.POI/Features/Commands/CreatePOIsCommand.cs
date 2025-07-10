1ef8141ca15b9da5d23378bbb53d7c7d72ad37f8
Johan Ferguson <johan.ferguson@gmail.com>
2025-07-10 21:20:01 +0200

    v1

---
+++ GeoInt.Application.POI/Features/Commands/CreatePOIsCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoInt.Application.POI.Features.Commands
+{
+    public class CreatePOIsCommand
+    {
+
+    }
+}
---
+++ GeoInt.Domain.POI/Entities/POIEntity.cs
@@ -0,0 +1,20 @@
+﻿using GeoInt.Core;
+
+
+namespace GeoInt.Domain.POI.Entities
+{
+    public class POIEntity : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public double Lat { get; set; }
+        public double Long { get; set; }
+
+        public DateTime created_at { get; set; }
+        public DateTime? modified_at { get; set; }
+        public DateTime? deleted_at { get; set; }
+    }
+}
